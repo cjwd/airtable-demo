b2daaaf116dc87221c3510d6b192402de6627749
cjwd <chinarakinda@gmail.com>
2018-12-02 08:14:56 -0400

    Babel loader 7 requires @babel/core

--- webpack.config.js
+++ webpack.config.js
@@ -90,7 +90,7 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: 'env',
+              presets: ['env'],
             }
           }
         ]
